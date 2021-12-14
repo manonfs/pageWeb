cc8c19453ef70daf14c64a614dee4da85c130683
Manon Fernandes <m.fernandes3108@gmail.com>
2021-12-14 16:36:02 +0100

    Create accueil.php
    fichier

---
+++ accueil.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="with=device-width, initial-scale=1.0">
+	<title>Exercice GitHub</title>
+	<link rel="stylesheet" type="text/css" href="MasterGcell1.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+     <div class="header">
+	    <div class="navbar">
+		    <ul>
+				<!--<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>-->
+				<li><a href="accueil.php"><i class="fa fa-home"></i> Accueil</a>
+				<li><a href="Connexion.php"><i class="fa fa-user-o"></i> Se connecter</a>
+					<!--<div class="sub-menu1">
+						<ul>
+							<li><a href="#">Etudiant</a></li>
+							<li><a href="#">Administrateur</a></li>							
+						</ul>
+					</div>-->
+				</li>
+            </ul>
+			<!--<div class="navbar-icons">
+				<img src="image/profil.png">
+					<div class="sub-menu1">
+						<ul>
+							<li><a href="#">Se connecter</a></li>
+						</ul>
+					</div>
+			</div>-->
+		</div>
+		<div class="banner">
+			<div class="left-column">
+				<!--<div class="search-box">
+					<img src="image/Recherche.png">
+					<input type="text">
+				</div>-->
+				<br>
+				<br>
+				<br>
+				<h1>Git<span>Hub</span></h1>
+				<h3><span>14  Décembre 2021</span></h3>
+
+				<!--<div class="btn">
+					<button type="button" class="primary-btn">Lire...</button>
+					<!--<button type="button">Watch video</button>
+				</div>-->
+				
+			</div>
+			<div class="right-column">
+				<img src="image/Master.jpg">
+								<fieldset>
+					<br>
+					
+					<div>
+
+					
+						
+					</font></td>
+						</tr>
+					</table>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+	 </div>	
+</body>
+<br>
+<br>
+<br>
+<body>	
+<footer style="position: absolute;left: 0;right: 0; background: #BDD9E8; height: 170px; width: 98.8vw;color: #000; ">
+	<div class="footer-content">
+	    <br>
+		<h4>GitHub</h4>
+		<ul1 class="map">
+			<li><a><i class="fa fa-map-marker"></i></a></li> B2, 86000 Poitiers
+        </ul1>
+		<ul1 class="contact">
+			<li><a><i class="fa fa-facebook-square"></i></a>  GPHY</li>
+			<li><a><i class="fa fa-phone"></i></a>  14 Novembre 2021 </li>
+			<li><a><i class="fa fa-envelope-o"></i></a></li>
+		</ul1>
+	</div>
+</footer>
+
+</body>
+</html>
+
+
+
